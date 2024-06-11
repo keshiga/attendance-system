69ea961f55115b185869c0c6c9f7023bb4eae379
keshiga <64153198+keshiga@users.noreply.github.com>
2024-06-11 15:49:16 +0530

    Create JwtUtil.java

---
+++ server/src/main/java/com/example/attendance_system/util/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.example.attendance_system.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+//import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+	private String secret = "secret";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String validateTokenAndGetUsername(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+	
+}
