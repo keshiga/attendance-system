d93b61898193ac335b45550b60354a0b1b4bef7e
keshiga <64153198+keshiga@users.noreply.github.com>
2024-06-11 15:43:03 +0530

    Create AuthController.java

---
+++ server/src/main/java/com/example/attendance_system/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.example.attendance_system.controller;
+
+import com.example.attendance_system.model.User;
+import com.example.attendance_system.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.attendance_system.service.AuthService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+
+        if (authService.authenticate(user.getUsername(), user.getPassword())) {
+            String token = jwtUtil.generateToken(user.getUsername());
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(401).body("Invalid credentials");
+        }
+    }
+
+}
